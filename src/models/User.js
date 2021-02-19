32ee67875bdd0d0ec4d333a3bc0b5c21fa6db6dd
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-02-19 17:26:48 +0000

    added user model with watchlist in scheema

---
+++ src/models/User.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema({
+    "name": {
+        type: String,
+        required: true
+    },
+    "email": {
+        type: String,
+        required: true,
+        unique: true
+    },
+    "password": {
+        type: String,
+        required: true
+    },
+    "watchlist": [
+        {ticker: {type: String}}
+    ],
+    "tokens": [
+        {token: {type: String}}
+    ]
+});
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email})
+    if(!user){
+        throw new Error("Unable to login")
+    }
+    const passwordsMatch = await bcrypt.compare(password, user.password)
+    if (!passwordsMatch){
+        throw new Error("Unable to login")
+    }
+    return user 
+}
+
+userSchema.methods.toJSON = async function() {
+    const userObject = this.toObject();
+    delete userObject.password;
+    delete userObject.tokens;
+    return userObject;
+
+}
+
+userSchema.methods.generateAuthToken = async function() {
+    console.log("gen auth called")
+    const token = jwt.sign({_id: this._id}, process.env.SECRET, {expiresIn: "1 week"})
+    this.tokens.push({token})
+    return token;
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+}
