047d8ec1913cf11ec44f6129683201a4d0874446
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-02-19 16:51:35 +0000

    added server.js file and connected to the mongo database

---
+++ src/db/connection.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('successfully connected to mongodb')
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+connection();
---
+++ src/server.js
@@ -0,0 +1,22 @@
+require("./db/connection");
+const express = require("express");
+const cors = require('cors')
+// const { Post } = require("./models/Post");
+// const { userRouter } = require("./routes/users");
+// const { postRouter } = require("./routes/posts");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+// app.use(userRouter);
+// app.use(postRouter);
+
+app.get("/health", (req, res) => {
+  res.status(200).send({ message: "API is working" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
