5370895902f0f99a1cef95d55d61fee02d580e75
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-02-22 12:18:15 +0000

    Created Post model and added username to user model

--- src/models/User.js
+++ src/models/User.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    "username": {
+        type: String,
+        required: true,
+        unique: true
+    },
     "watchlist": [
         {ticker: {type: String}}
     ],
